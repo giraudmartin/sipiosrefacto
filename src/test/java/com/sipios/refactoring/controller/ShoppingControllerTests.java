1da6d61bd0778a4191ab48ea1a013f2e572d2170
martin.giraud <>
2022-09-19 15:32:12 +0200

    initial commit

---
+++ src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
@@ -0,0 +1,19 @@
+package com.sipios.refactoring.controller;
+
+import com.sipios.refactoring.UnitTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+class ShoppingControllerTests extends UnitTest {
+
+    @InjectMocks
+    private ShoppingController controller;
+
+    @Test
+    void should_not_throw() {
+        Assertions.assertDoesNotThrow(
+            () -> controller.getPrice(new Body(new Item[] {}, "STANDARD_CUSTOMER"))
+        );
+    }
+}
